ac2e708f6a1c4a8332b58dfc346bcb9d0587eee1
irefusetosleep <68473299+irefusetosleep@users.noreply.github.com>
2025-08-07 17:40:31 +0100

    sigma

---
+++ README.md
@@ -0,0 +1,7 @@
+make sure u have python and pip installed
+
+run "RUN THIS FIRST.bat"
+
+run main.py
+
+tab into roblox WHILE you're in your inner world (if u arent a soul reaper then set the "SOUL_REAPER" value to False on the first line of main.py)
---
+++ main.py
@@ -0,0 +1,60 @@
+SOUL_REAPER = True
+
+import time
+import pydirectinput
+import cv2
+import pyautogui
+import numpy as np
+import random
+
+template = cv2.imread("Raid_Yes.png", cv2.IMREAD_COLOR)
+
+_, w, h = template.shape[::-1]
+
+def search(): #this function seaches for the "Yes" button when called, returns x and y coords of location:wq
+    try:
+        location = pyautogui.locateCenterOnScreen("Raid_Yes.png", confidence=.5)
+        if location is not None:
+            return location
+    except pyautogui.ImageNotFoundException:
+        print("Image not found, retrying")
+
+    return (-69, -69)    
+    
+def click(x, y): #youll never guess what this function does
+    print("Clicking yes")
+    print("MOVING MOUSE TO",x,y)
+    for i in range(1, 5):
+        pyautogui.moveTo(x + random.randint(-15, 15), y)
+        pydirectinput.click()
+        time.sleep(.2)
+
+print("Starting the macro, tab into roblox.")
+time.sleep(3)
+
+time_since_moved = time.time()
+
+time_to_move = 10 * 60 #15 minutes
+
+while True:
+    time.sleep(.1)
+
+    if time.time() - time_since_moved > time_to_move:
+        pydirectinput.press("Space") #built in anti-afk kick!
+        time_since_moved = time.time()
+
+    x, y = search()
+
+    if x != -69: #-69 means it didnt find it
+        click(x, y)
+
+        if SOUL_REAPER:
+            time.sleep(95) #A raid takes 90 seconds to start
+            pydirectinput.press("M")
+            time.sleep(5)
+            pydirectinput.press("M")
+    else:
+        print("Couldnt find yes button")
+        
+
+
